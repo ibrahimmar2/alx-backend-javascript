3547e67adb3dc5b7145ec136643499b4fd8ad6f3
ibrahim <irahimammar811@gmail.com>
2024-08-07 15:54:08 +0300

    Task 6-final-user.js is Done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,24 @@
+#!/usr/bin/env node
+
+/*
+ * full user signup functionality
+*/
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+  const sUPromise = signUpUser(firstName, lastName);
+  const uPPromise = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  return Promise.allSettled([sUPromise, uPPromise]).then((responses) => {
+    const returnValue = [];
+    responses.forEach((response) => {
+      if (response.status !== 'fulfilled') {
+        returnValue.push({ status: response.status, value: `${response.reason}` });
+      } else {
+        returnValue.push({ status: response.status, value: response.value });
+      }
+    });
+    return returnValue;
+  });
 }
