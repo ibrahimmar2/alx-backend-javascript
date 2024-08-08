4f8e17df70a0e2cc7172f1630c3e7205b17c5da2
ibrahim <irahimammar811@gmail.com>
2024-08-08 18:32:32 +0300

    Task 8-hbtn_class.js  is Done

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,28 @@
-export default class HolbertonClass {
-  constructor(size, location) {
-    this.size = size;
-    this.location = location;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    this._location = value;
-  }
-
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this.size;
+class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-    if (hint === 'number') {
-      return this.location;
+    this.sqft = sqft;
+  }
+
+  /**
+     * @param {Number} sqft
+     */
+  set sqft(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft nust be a number');
     }
-    return this;
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 }
+
+export default Building;
