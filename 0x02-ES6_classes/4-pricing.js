24fe3782868b23c2fa8389cbeaa3828fd1ccfcd8
ibrahim <irahimammar811@gmail.com>
2024-08-08 18:01:41 +0300

    Task 4-pricing.js is Done

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,44 +1,59 @@
 import Currency from './3-currency';
 
-export default class Pricing {
-  constructor(amount, curreny) {
+class Pricing {
+  constructor(amount, currency) {
     this.amount = amount;
-    this.curreny = curreny;
+    this.currency = currency;
+  }
+
+  /**
+   * @param {Number} amount
+   */
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a Number');
+    }
+    this._amount = amount;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('amount must be a number');
+  /**
+   * @param {Currency} currency
+   */
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency must be an instance of Currency');
     }
-    this._amount = value;
   }
 
-  get curreny() {
-    return this._curreny;
-  }
-
-  set curreny(value) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError('currency must be a Currency');
-    }
-    this._curreny = value;
+  get currency() {
+    return this._currency;
   }
 
   displayFullPrice() {
-    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
+  /**
+   * @param {Number} amount
+   * @param {Number} conversionRate
+   */
   static convertPrice(amount, conversionRate) {
     if (typeof amount !== 'number') {
       throw new TypeError('amount must be a number');
     }
+
     if (typeof conversionRate !== 'number') {
       throw new TypeError('conversionRate must be a number');
     }
+
     return amount * conversionRate;
   }
 }
+
+export default Pricing;
